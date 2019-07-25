c2ed193377e9043535df67d560b88b229bc647f3
Jérémy OUDOT <53262474+ad-vitam-aeternam@users.noreply.github.com>
2019-07-25 11:54:36 +0200

    Problème NAV accueil

--- datas/md-list.js
+++ datas/md-list.js
@@ -1,7 +1,7 @@
 BLOG.aData = [
     {
         title: '01 - Ennui',
-        description: 'Je la hais. Je me languille d\'impatience toute la journée afin d\'avoir un peu de temps pour moi, et quand ce moment est enfin à ma porté, elle se manifeste ...',
+        description: 'Je la hais. Je me languis d\'impatience toute la journée afin d\'avoir un peu de temps pour moi, et quand ce moment est enfin à ma porté, elle se manifeste ...',
         markdown: '01-ennui.md',
         image: 'https://images.pexels.com/photos/2234623/pexels-photo-2234623.jpeg?auto=compress&cs=tinysrgb'
     }
