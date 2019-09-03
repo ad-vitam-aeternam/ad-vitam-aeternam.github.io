084a31896ee9949f167b1e37aed4b11717d31422
Jérémy OUDOT <53262474+ad-vitam-aeternam@users.noreply.github.com>
2019-09-03 09:54:42 +0200

    Add files via upload

--- datas/md-list.js
+++ datas/md-list.js
@@ -1,13 +1,13 @@
 APP.aData = [
     {
-        title: '01 - Ennui',
-        description: 'Je le hais. Je me languis d\'impatience toute la journée afin d\'avoir un peu de temps pour moi, et quand ce moment est enfin à ma porté, il se manifeste. Un moment d\'égarement et il est là, devant moi, provocant et cruel ...',
+        title: 'Je le hais',
+        description: 'Je me languis d\'impatience toute la journée afin d\'avoir un peu de temps pour moi, et quand ce moment est enfin à ma porté, il se manifeste. Un moment d\'égarement et il est là, devant moi, provocant et cruel ...',
         markdown: '01-ennui.md',
         image: 'https://images.pexels.com/photos/2234623/pexels-photo-2234623.jpeg?auto=compress&cs=tinysrgb'
     },
     {
-        title: '02 - Solitude',
-        description: 'Je l\'ai choisi. Une bonne bouffée d\'air frais que de se retrouver seul, en pleine nature, bruit léger, afin de réfléchir. Cela me permet de méditer sur les efforts qu\'ils me restent à faire, de réfléchir ou d\'inventer ...',
+        title: 'Je l\'ai choisi',
+        description: 'Une bonne bouffée d\'air frais que de se retrouver seul, en pleine nature, bruit léger, afin de réfléchir. Cela me permet de méditer sur les efforts qu\'ils me restent à faire, de réfléchir ou d\'inventer ...',
         markdown: '02-solitude.md',
         image: 'https://images.pexels.com/photos/1365211/pexels-photo-1365211.jpeg?auto=compress&cs=tinysrgb'
     }
