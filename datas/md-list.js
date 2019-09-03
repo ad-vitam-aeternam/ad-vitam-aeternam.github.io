c02b42bade7ebd67115051985c4c57606ed6fe58
Jérémy OUDOT <53262474+ad-vitam-aeternam@users.noreply.github.com>
2019-09-03 11:26:29 +0200

    Add files via upload

---
+++ datas/md-list.js
@@ -0,0 +1,14 @@
+APP.aData = [
+    {
+        title: 'Je le hais',
+        description: 'Je me languis d\'impatience toute la journée afin d\'avoir un peu de temps pour moi, et quand ce moment est enfin à ma porté, il se manifeste. Un moment d\'égarement et il est là, devant moi, provocant et cruel ...',
+        markdown: '01-je-le-hais.md',
+        image: 'https://images.pexels.com/photos/2234623/pexels-photo-2234623.jpeg?auto=compress&cs=tinysrgb'
+    },
+    {
+        title: 'Je l\'ai choisi',
+        description: 'Une bonne bouffée d\'air frais que de se retrouver seul, en pleine nature, bruit léger, afin de réfléchir. Cela me permet de méditer sur les efforts qu\'ils me restent à faire, de réfléchir ou d\'inventer ...',
+        markdown: '02-je-l-ai-choisi.md',
+        image: 'https://images.pexels.com/photos/1365211/pexels-photo-1365211.jpeg?auto=compress&cs=tinysrgb'
+    }
+];
